e89b7eb71687c17017d22d70f8e74986920d8d78
Sulav Gautam <sulavgautam2071@gmail.com>
2025-11-07 11:15:40 -0500

    added schemas files

---
+++ backend/schemas/potion_schema.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel, Field, ConffigDict
+from backend.models.potion_model import Rarity, Potion
+
+# Base
