787d96cb0e218f7f73f71564de2720f7b604f035
Sulav Gautam <sulavgautam2071@gmail.com>
2025-11-07 10:37:31 -0500

    added user and potions seed file

--- backend/seed.py
+++ backend/seed.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models.user import User
@@ -10,3 +10,41 @@ from database import Base, DATABASE_URL
 # Create the database engine and session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def seed_database(drop=True):
+    # Create all tables
+    if drop:
+        print("Dropping existing database tables...")
+        Base.metadata.drop_all(bind=engine)
+    
+    Base.metadata.create_all(bind=engine)
+    print("Tables created successfully.")
+
+    # Create a new session
+    db = SessionLocal()
+
+    try:
+        # Seed Users
+        print("clearing existinng rows...")
+        db.query(Discovery).delete()
+        db.query(Potion).delete()
+        db.query(User).delete()
+        db.commit()
+
+        print("Inserting seed rows...")
+
+        user = User(username="player_one", display_name="Player One")
+        db.add(user)
+        db.flush()  #get user.id
+
+        # Potions
+        p1 = Potion(
+            name="Whisker Draught of Mist",
+            rarity = Rarity.COMMON,
+            effects=["Luck +1"],
+            essenses={"weather": "mist", "prime": False}
+            signature_hash="sig_common_001",
+            gold_award=5,
+            xp_award=5,
+        )
+        
