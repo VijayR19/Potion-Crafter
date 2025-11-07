856a0c27be5972edd496a223f930bf6638be6ab9
Sulav Gautam <sulavgautam2071@gmail.com>
2025-11-07 14:23:16 -0500

    added get id user

--- backend/crud/user_crud.py
+++ backend/crud/user_crud.py
@@ -7,3 +7,6 @@ from schemas.user_schema import UserRead
 
 def get_all_users(db: Session) -> List[User]:
     return db.query(User).all()
+
+def get_user_by_id(db: Session, user_id: int) -> User:
+    return db.query(User).filter(User.id == user_id).first()
--- backend/routes/user_route.py
+++ backend/routes/user_route.py
@@ -1,6 +1,7 @@
 from typing import List
 from fastapi import APIRouter, status
 from dependencies import db_dependency
+from fastapi import HTTPException
 
 from schemas.user_schema import UserRead
 from crud import user_crud
@@ -10,3 +11,11 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.get("/", response_model=List[UserRead], status_code=status.HTTP_200_OK)
 def read_users(db: db_dependency) -> List[UserRead]:
     return user_crud.get_all_users(db)
+
+# Get '/{user_id}'
+@router.get("/{user_id}", response_model=UserRead, status_code=status.HTTP_200_OK)
+def read_user(user_id: int, db: db_dependency) -> UserRead:
+    user = user_crud.get_user_by_id(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user 
